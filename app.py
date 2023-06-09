58c5414ea58a4ae25aa6ae40d6be04d351f8ad3b
Gaganish <135037387+gaganish@users.noreply.github.com>
2023-06-10 00:24:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-from tensorflow import keras
-import tensorflow
-import matplotlib.pyplot as plt
-import cv2
-import numpy as np
 from flask import Flask, render_template, request
-from tensorflow.keras.utils import load_img, img_to_array
-from werkzeug.wrappers import Request, Response
-
 app=Flask(__name__)
 
 @app.route('/',methods=['GET'])
