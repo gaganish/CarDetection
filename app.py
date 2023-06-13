6a469678678d0c0a68cf4d90d7ee93c3bdf3942d
Gaganish <135037387+gaganish@users.noreply.github.com>
2023-06-13 18:07:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def pred():
     return render_template('index.html', prediction=classification, detectedimage=detectedpath)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True, host="0.0.0.0")
